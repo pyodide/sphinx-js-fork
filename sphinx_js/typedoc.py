46aaf40aa7cac8e8a38401b07c362f9685658d67
Hood Chatham <roberthoodchatham@gmail.com>
2023-02-13 12:04:48 -0800

    Add getSignature and setSignature to index

--- sphinx_js/typedoc.py
+++ sphinx_js/typedoc.py
@@ -90,11 +90,14 @@ class Converter:
         if parent and isinstance(node, Signature):
             node.parent_member_properties = parent.member_properties()
 
-        # Burrow into everything that could contain more ID'd items. We don't
-        # need setSignature or getSignature for now. Do we need indexSignature?
+        # Burrow into everything that could contain more ID'd items
         children: list[Sequence[IndexType]] = []
 
         children.append(node.children)
+        if isinstance(node, Accessor):
+            children.append(node.getSignature)
+            children.append(node.setSignature)
+
         if isinstance(node, Callable):
             children.append(node.signatures)
 
@@ -522,7 +525,7 @@ class ExternalModule(NodeBase):
 
 class OtherNode(NodeBase):
     kindString: Literal[
-        "Enumeration", "Enumeration member", "Namespace", "Type alias", "Reference"
+        "Enumeration", "Enumeration Member", "Namespace", "Type alias", "Reference"
     ]
 
 
