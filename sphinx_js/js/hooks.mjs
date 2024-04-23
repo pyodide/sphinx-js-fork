ed6564456409b3ec300c60c289d3fcdfc315a7a1
Hood Chatham <roberthoodchatham@gmail.com>
2024-04-23 17:21:50 +0200

    Add comments

--- sphinx_js/js/hooks.mjs
+++ sphinx_js/js/hooks.mjs
@@ -1,12 +1,18 @@
+// An import hook to pick up packages in the node_modules that typedoc is
+// installed into
+
 export async function resolve(specifier, context, nextResolve) {
   // Take an `import` or `require` specifier and resolve it to a URL.
   try {
     return await nextResolve(specifier, context);
   } catch (e) {
     if (e.code !== "ERR_MODULE_NOT_FOUND") {
+      // Unusual error let it propagate
       throw e;
     }
   }
+  // Try resolving again with respect to the directory that typedoc is installed
+  // into
   context.parentURL = `file:${process.env["TYPEDOC_NODE_MODULES"]}/`;
   return await nextResolve(specifier, context);
 }
