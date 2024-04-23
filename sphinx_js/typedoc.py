0a49d431892b38e8cd8de7014784fd194176e229
Hood Chatham <roberthoodchatham@gmail.com>
2024-04-23 16:00:53 +0200

    Debug

--- sphinx_js/typedoc.py
+++ sphinx_js/typedoc.py
@@ -93,6 +93,7 @@ def typedoc_output(
     os.environ["NODE_PATH"] = str(Path(typedoc).parents[3])
     print("NODE_PATH=", str(Path(typedoc).parents[3]))
     print([x.name for x in Path(typedoc).parents[3].glob("*")])
+    print([x.name for x in Path(typedoc).parents[2].glob("*")])
     command = Command("node")
     command.add(str(Path(__file__).parent / "call_typedoc.mjs"))
     command.add("--entryPointStrategy", "expand")
