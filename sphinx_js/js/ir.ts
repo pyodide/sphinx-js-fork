403b80f9776a8cdbc09899465b57a739379d3e92
Hood Chatham <roberthoodchatham@gmail.com>
2024-05-03 18:35:21 +0200

    Introduce a type error

--- sphinx_js/js/ir.ts
+++ sphinx_js/js/ir.ts
@@ -134,7 +134,7 @@ export type _MembersAndSupers = {
 export type Interface = TopLevel &
   _MembersAndSupers & {
     type_params: TypeParam[];
-    kind: "interface";
+    kind: "interfaces";
   };
 
 export type Class = TopLevel &
