16ba3f61995652a68b4152bf1700f78194fe8ea4
Christian Clauss <cclauss@me.com>
2024-11-14 14:39:04 +0100

    setup.py: Upgrade to markupsafe==3.0.2 (#170)

--- setup.py
+++ setup.py
@@ -18,9 +18,7 @@ setup(
         "Jinja2>2.0",
         "parsimonious>=0.10.0,<0.11.0",
         "Sphinx>=4.1.0",
-        # Pin markupsafe because of
-        # https://github.com/pallets/jinja/issues/1585
-        "markupsafe==2.0.1",
+        "markupsafe==3.0.2",
         "attrs",
         "cattrs",
     ],
