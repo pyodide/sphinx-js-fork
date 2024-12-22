b02a30a80cb72349b35ce3c27eaee64f9b3a2c33
Hood Chatham <roberthoodchatham@gmail.com>
2024-12-22 17:34:41 +0100

    Add some typescript tests (#173)
    So far these more or less duplicate a subset of the tests in
    `test_typedoc_analysis` so they don't provide that much added value because
    they are still basically integration tests with respect to the typescript
    logic. But this is a first step and will allow finer grained typescript tests
    in the future.

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from textwrap import dedent
 
 import nox
 from nox.sessions import Session
+
+PROJECT_ROOT = Path(__file__).parent
 
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
@@ -25,15 +28,41 @@ def tests(session: Session) -> None:
 @nox.session(python=["3.12"])
 @nox.parametrize("typedoc", ["0.25", "0.26"])
 def test_typedoc(session: Session, typedoc: str) -> None:
+    # Install python dependencies
     session.install("-r", "requirements_dev.txt")
     venvroot = Path(session.bin).parent
-    (venvroot / "node_modules").mkdir()
+    node_modules = (venvroot / "node_modules").resolve()
+    node_modules.mkdir()
     with session.chdir(venvroot):
+        # Install node dependencies
         session.run(
-            "npm", "i", "--no-save", "jsdoc@4.0.0", f"typedoc@{typedoc}", external=True
+            "npm",
+            "i",
+            "--no-save",
+            "tsx",
+            "jsdoc@4.0.0",
+            f"typedoc@{typedoc}",
+            external=True,
         )
         session.run("npx", "tsc", "--version", external=True)
         session.run("npx", "typedoc", "--version", external=True)
+        # Run typescript tests
+        test_file = (PROJECT_ROOT / "tests/test.ts").resolve()
+        register_import_hook = PROJECT_ROOT / "sphinx_js/js/registerImportHook.mjs"
+        ts_tests = Path(venvroot / "ts_tests")
+        # Write script to a file so that it is easy to rerun without reinstalling dependencies.
+        ts_tests.write_text(
+            dedent(
+                f"""\
+                #!/bin/sh
+                TYPEDOC_NODE_MODULES={venvroot} node --import {register_import_hook} --import {node_modules/"tsx/dist/loader.mjs"} --test {test_file}
+                """
+            )
+        )
+        ts_tests.chmod(0o777)
+        session.run(ts_tests, external=True)
+
+    # Run Python tests
     session.run("pytest", "--junitxml=test-results.xml", "-k", "not js")
 
 
